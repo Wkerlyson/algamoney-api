28cece81bb78684ad9ac5de726e4e8ff71e9c504
wkerlyson Batista <wkerlyson.batista@gmail.com>
2019-07-12 17:04:49 -0300

    adding create method in CategoryResource

--- src/main/java/com/project/algamoneyapi/resource/CategoryResource.java
+++ src/main/java/com/project/algamoneyapi/resource/CategoryResource.java
@@ -1,11 +1,20 @@
 package com.project.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.algamoneyapi.model.Category;
 import com.project.algamoneyapi.repository.CategoryRepository;
@@ -20,5 +29,21 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> list(){
 		return repository.findAll(); 
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category categorySalved = repository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+			.buildAndExpand(categorySalved.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categorySalved);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Category> findById(@PathVariable Long id) {
+		return repository.findById(id);
 	}
 }
