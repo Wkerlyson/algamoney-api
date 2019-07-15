447d43122412f84015f7c7a4e57156b417032482
wkerlyson Batista <wkerlyson.batista@gmail.com>
2019-07-15 20:20:58 -0300

    adding Person model

---
+++ src/main/java/com/project/algamoneyapi/model/Address.java
@@ -0,0 +1,75 @@
+package com.project.algamoneyapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	
+	@Column(name = "logradouro")
+	private String publicPlace;
+	
+	@Column(name = "numero")
+	private String number;
+	
+	@Column(name = "complento")
+	private String complement;
+	
+	@Column(name = "bairro")
+	private String district;
+	
+	@Column(name = "cep")
+	private String zipCore;
+	
+	@Column(name = "cidade")
+	private String city;
+	
+	@Column(name = "estado")
+	private String state;
+	
+	
+	public String getPublicPlace() {
+		return publicPlace;
+	}
+	public void setPublicPlace(String publicPlace) {
+		this.publicPlace = publicPlace;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getComplement() {
+		return complement;
+	}
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getZipCore() {
+		return zipCore;
+	}
+	public void setZipCore(String zipCore) {
+		this.zipCore = zipCore;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}
